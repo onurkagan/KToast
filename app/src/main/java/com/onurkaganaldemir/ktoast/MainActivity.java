4a8521441536f767117e37320c12dcd59b1db2bc
onurkagan <onurkaganaldemir@gmail.com>
2018-02-25 23:57:24 +0300

    v1.0.0 Sample

--- app/src/main/java/com/onurkaganaldemir/ktoast/MainActivity.java
+++ app/src/main/java/com/onurkaganaldemir/ktoast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.onurkaganaldemir.ktoast;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -14,11 +15,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.mainActivityBtnToast).setOnClickListener(new View.OnClickListener() {
+        // Success
+        findViewById(R.id.mainActivityBtnSuccess).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                KToast.successToast(MainActivity.this ,"You win !", Gravity.BOTTOM, KToast.LENGTH_AUTO);
+                KToast.successToast(MainActivity.this, "This is a success toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO);
             }
         });
+
+        // Info
+        findViewById(R.id.mainActivityBtnInfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.infoToast(MainActivity.this, "This is a info toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO);
+            }
+        });
+
+        // Normal
+        findViewById(R.id.mainActivityBtnNormal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.normalToast(MainActivity.this, "This is a normal toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO, R.drawable.ic_infinite_white);
+            }
+        });
+
+        // Warning
+        findViewById(R.id.mainActivityBtnWarning).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.warningToast(MainActivity.this, "This is a warning toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO);
+            }
+        });
+
+        // Error
+        findViewById(R.id.mainActivityBtnError).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.errorToast(MainActivity.this, "This is a error toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO);
+            }
+        });
+
+        // Custom Color
+        findViewById(R.id.mainActivityBtnCustomColor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.customColorToast(MainActivity.this, "This is a custom color toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO, R.color.fuchsia, R.drawable.ic_infinite_white);
+            }
+        });
+
+        // Custom Drawable
+        findViewById(R.id.mainActivityBtnCustomDrawable).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KToast.customBackgroudToast(MainActivity.this, "This is a custom drawable toast.", Gravity.BOTTOM, KToast.LENGTH_AUTO, R.drawable.background_toast, null ,R.drawable.ic_infinite_white);
+            }
+        });
+
     }
 }
