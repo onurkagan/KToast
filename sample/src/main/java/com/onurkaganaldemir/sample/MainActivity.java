4e3081b2edd4c7c0e7e1e3ca29a77868c55e39da
maxis7567 <mahdi.sy1994@outlook.com>
2019-03-06 16:47:25 +0330

    Migrate android x replace a solid color insteadof shape tint -> fix background color for api 16

--- sample/src/main/java/com/onurkaganaldemir/sample/MainActivity.java
+++ sample/src/main/java/com/onurkaganaldemir/sample/MainActivity.java
@@ -1,6 +1,6 @@
 package com.onurkaganaldemir.sample;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
